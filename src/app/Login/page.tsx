5b528d45b7aa2b0ed34ae3b38a52750cbebfa257
faithfuldao <victor.dubrana2@gmail.com>
2024-06-07 09:54:15 +0200

    Login Clean Css

--- src/app/Login/page.tsx
+++ src/app/Login/page.tsx
@@ -30,254 +30,69 @@ export default async function Login(props: LoginProps) {
   const posts = await getPosts();
   console.log({ posts });
 
-  const renderContent = () => {
-    switch (languageActive) {
-      case "french":
-        return (
-          <div className="flex h-screen w-full flex-col bg-gradient-to-t from-linear2 to-linear1">
-            <div className="h-1/7 items-centee m-10 mr-20 flex w-full flex-row">
-              <div className="w-4/6"></div>
-              <div className="flex w-2/6 items-center justify-center space-x-10">
-                <button>
-                  <Image
-                    alt="frenchIcon"
-                    src={frenchIcon}
-                    height={30}
-                    width={30}
-                  />
-                </button>
-                <button>
-                  <Image
-                    alt="englishIcon"
-                    src={englishIcon}
-                    height={30}
-                    width={30}
-                  />
-                </button>
-                <button>
-                  <Image
-                    alt="spanishIcon"
-                    src={spanishIcon}
-                    height={30}
-                    width={30}
-                  />
-                </button>
-                <button>
-                  <Image
-                    alt="chineseIcon"
-                    src={chineseIcon}
-                    height={30}
-                    width={30}
-                  />
-                </button>
-              </div>
-            </div>
-            <div className="flex h-screen flex-row-reverse">
-              <div className="center-items mt-15 mr-20 flex h-4/5 w-6/12 flex-col justify-center rounded-2xl bg-whiteBackground p-5">
-                <div className="flex flex-col items-center">
-                  <Image
-                    className="mb-20"
-                    alt="Moodle"
-                    src={moodleConnexionIcon}
-                  />
-                  <div className="flex flex-row">
-                    <div className="flex flex-col">
-                      <div className="mb-8">
-                        <div className="text-xl font-extrabold text-fontBlack">
-                          Identifiant
-                        </div>
-                        <div className="border-1 flex flex-row border-b border-fontBlack p-1">
-                          <Image alt="userIcon" src={userIcon} width={25} />
-                          <input
-                            className="border-0 bg-whiteBackground text-fontGray"
-                            placeholder="Insérez votre Identifiant"
-                          />
-                        </div>
-                      </div>
-                      <div className="mb8">
-                        <div className="text-xl font-extrabold text-fontBlack">
-                          Mot de Passe
-                        </div>
-                        <div className="border-1 flex flex-row border-b border-fontBlack p-1">
-                          <Image alt="doorIcon" src={doorIcon} width={25} />
-                          <input
-                            className="bg-whiteBackground text-fontGray"
-                            placeholder="Insérez votre mot de passe"
-                          />
-                          <button>
-                            <Image alt="arrowIcon" src={arrowIcon} width={30} />
-                          </button>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <ul>
-                  {posts.map((post, index) => (
-                    <li key={index} className="text-lg">
-                      {post.name}
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            </div>
-          </div>
-        );
-      case "english":
-        return (
-          <div className="flex h-screen w-full flex-col bg-gradient-to-t from-linear2 to-linear1">
-            <div className="h-1/7 items-centee m-10 mr-20 flex w-full flex-row">
-              <div className="w-4/6"></div>
-              <div className="flex w-2/6 items-center justify-center space-x-10">
-                <button>
-                  <Image
-                    alt="frenchIcon"
-                    src={frenchIcon}
-                    height={30}
-                    width={30}
-                  />
-                </button>
-                <button>
-                  <Image
-                    alt="englishIcon"
-                    src={englishIcon}
-                    height={30}
-                    width={30}
-                  />
-                </button>
-                <button>
-                  <Image
-                    alt="spanishIcon"
-                    src={spanishIcon}
-                    height={30}
-                    width={30}
-                  />
-                </button>
-                <button>
-                  <Image
-                    alt="chineseIcon"
-                    src={chineseIcon}
-                    height={30}
-                    width={30}
-                  />
-                </button>
-              </div>
-            </div>
-            <div className="flex h-screen flex-row-reverse">
-              <div className="center-items mt-15 mr-20 flex h-4/5 w-6/12 flex-col justify-center rounded-2xl bg-whiteBackground p-5">
-                <div className="flex flex-col items-center">
-                  <Image
-                    className="mb-20"
-                    alt="Moodle"
-                    src={moodleConnexionIcon}
-                  />
-                  <div className="flex flex-row">
-                    <div className="flex flex-col">
-                      <div className="mb-8">
-                        <div className="text-xl font-extrabold text-fontBlack">
-                          Identifiant
-                        </div>
-                        <div className="border-1 flex flex-row border-b border-fontBlack p-1">
-                          <Image alt="userIcon" src={userIcon} width={25} />
-                          <input
-                            className="border-0 bg-whiteBackground text-fontGray"
-                            placeholder="Insérez votre Identifiant"
-                          />
-                        </div>
-                      </div>
-                      <div className="mb8">
-                        <div className="text-xl font-extrabold text-fontBlack">
-                          Mot de Passe
-                        </div>
-                        <div className="border-1 flex flex-row border-b border-fontBlack p-1">
-                          <Image alt="doorIcon" src={doorIcon} width={25} />
-                          <input
-                            className="bg-whiteBackground text-fontGray"
-                            placeholder="Insérez votre mot de passe"
-                          />
-                          <button>
-                            <Image alt="arrowIcon" src={arrowIcon} width={30} />
-                          </button>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <ul>
-                  {posts.map((post, index) => (
-                    <li key={index} className="text-lg">
-                      {post.name}
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            </div>
-          </div>
-        );
-    }
-  };
   return (
     <div className="flex h-screen w-full flex-col bg-gradient-to-t from-linear2 to-linear1">
-      <div className="h-1/7 items-centee m-10 mr-20 flex w-full flex-row">
-        <div className="w-4/6"></div>
-        <div className="flex w-2/6 items-center justify-center space-x-10">
+      <div className="h-1/7 items-centee m-10 mr-20 flex  flex-row">
+        <div className="w-full"></div>
+        <div className="flex  items-center justify-center space-x-10">
           <button>
-            <Image alt="frenchIcon" src={frenchIcon} height={30} width={30} />
+            <Image alt="frenchIcon" src={frenchIcon} height={45} width={45} />
           </button>
           <button>
-            <Image alt="englishIcon" src={englishIcon} height={30} width={30} />
+            <Image alt="englishIcon" src={englishIcon} height={45} width={45} />
           </button>
           <button>
-            <Image alt="spanishIcon" src={spanishIcon} height={30} width={30} />
+            <Image alt="spanishIcon" src={spanishIcon} height={45} width={45} />
           </button>
           <button>
-            <Image alt="chineseIcon" src={chineseIcon} height={30} width={30} />
+            <Image alt="chineseIcon" src={chineseIcon} height={45} width={45} />
           </button>
         </div>
       </div>
       <div className="flex h-screen flex-row-reverse">
-        <div className="center-items mt-15 mr-20 flex h-4/5 w-6/12 flex-col justify-center rounded-2xl bg-whiteBackground p-5">
-          <div className="flex flex-col items-center">
-            <Image className="mb-20" alt="Moodle" src={moodleConnexionIcon} />
+        <div className="center-items mr-20 flex h-4/5 w-6/12 flex-col justify-center rounded-2xl bg-whiteBackground">
+          <div className="flex flex-col items-center space-y-12">
+            <div>
+              <Image
+                className="mb-20"
+                alt="Moodle"
+                src={moodleConnexionIcon}
+                height={300}
+                width={400}
+              />
+            </div>
             <div className="flex flex-row">
               <div className="flex flex-col">
                 <div className="mb-8">
-                  <div className="text-xl font-extrabold text-fontBlack">
+                  <div className="text-2xl font-extrabold text-fontBlack">
                     Identifiant
                   </div>
-                  <div className="border-1 flex flex-row border-b border-fontBlack p-1">
-                    <Image alt="userIcon" src={userIcon} width={25} />
+                  <div className="border-1 flex flex-row space-x-4 border-b border-fontBlack p-2">
+                    <Image alt="userIcon" src={userIcon} width={35} />
                     <input
-                      className="border-0 bg-whiteBackground text-fontGray"
+                      className="border-0 bg-whiteBackground text-fontGray outline-none"
                       placeholder="Insérez votre Identifiant"
                     />
                   </div>
                 </div>
-                <div className="mb8">
-                  <div className="text-xl font-extrabold text-fontBlack">
+                <div className="mb-8">
+                  <div className=" text-2xl font-extrabold text-fontBlack">
                     Mot de Passe
                   </div>
-                  <div className="border-1 flex flex-row border-b border-fontBlack p-1">
-                    <Image alt="doorIcon" src={doorIcon} width={25} />
+                  <div className="border-1 flex flex-row space-x-4 border-b border-fontBlack p-2">
+                    <Image alt="doorIcon" src={doorIcon} width={35} />
                     <input
-                      className="bg-whiteBackground text-fontGray"
+                      className="bg-whiteBackground text-fontGray outline-none"
                       placeholder="Insérez votre mot de passe"
                     />
                     <button>
-                      <Image alt="arrowIcon" src={arrowIcon} width={30} />
+                      <Image alt="arrowIcon" src={arrowIcon} width={45} />
                     </button>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-          <ul>
-            {posts.map((post, index) => (
-              <li key={index} className="text-lg">
-                {post.name}
-              </li>
-            ))}
-          </ul>
         </div>
       </div>
     </div>
