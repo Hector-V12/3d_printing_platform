82fea83cf16ed7065decd357e7f7f1bf47b5a5c2
Hector_vza <h.vizzavona@orange.fr>
2024-06-10 01:29:34 +0200

    🐛 patch register

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -1,5 +1,3 @@
-// src/app/api/auth/register/route.ts
-
 import { NextRequest, NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcryptjs";
@@ -10,54 +8,62 @@ const prisma = new PrismaClient();
 /**
  * @swagger
  * /api/auth/register:
- *  post:
- *   description: Register a new user
- *  responses:
- *   201:
- *    description: User created successfully
- *  400:
- *   description: All fields are required
+ *   post:
+ *     description: Register a new user
+ *     responses:
+ *       201:
+ *         description: User created successfully
+ *       400:
+ *         description: All fields are required
  */
 
 export async function POST(req: NextRequest) {
-  const { email, password, name, surname, phoneNumber } = await req.json();
+  try {
+    const { email, password, name, surname, phoneNumber } = await req.json();
 
-  if (!email || !password || !name || !surname || !phoneNumber) {
+    if (!email || !password || !name || !surname || !phoneNumber) {
+      return NextResponse.json(
+        { message: "All fields are required" },
+        { status: 400 },
+      );
+    }
+
+    const existingUser = await prisma.user.findUnique({ where: { email } });
+
+    if (existingUser) {
+      return NextResponse.json(
+        { message: "User already exists" },
+        { status: 400 },
+      );
+    }
+
+    const hashedPassword = bcrypt.hashSync(password, 10);
+
+    const user = await prisma.user.create({
+      data: {
+        email,
+        password: hashedPassword,
+        name,
+        surname,
+        phoneNumber,
+      },
+    });
+
+    const token = jwt.sign(
+      { id: user.id, email: user.email },
+      process.env.JWT_SECRET as string,
+      { expiresIn: "1h" },
+    );
+
     return NextResponse.json(
-      { message: "All fields are required" },
-      { status: 400 },
+      { message: "User created successfully", token },
+      { status: 201 },
+    );
+  } catch (error) {
+    console.error("Error during user registration:", error);
+    return NextResponse.json(
+      { message: "Internal Server Error" },
+      { status: 500 },
     );
   }
-
-  const existingUser = await prisma.user.findUnique({ where: { email } });
-
-  if (existingUser) {
-    return NextResponse.json(
-      { message: "User already exists" },
-      { status: 400 },
-    );
-  }
-
-  const hashedPassword = bcrypt.hashSync(password, 10);
-
-  const user = await prisma.user.create({
-    data: {
-      email,
-      password: hashedPassword,
-      name,
-      surname,
-      phoneNumber,
-    },
-  });
-
-  const token = jwt.sign(
-    { id: user.id, email: user.email },
-    process.env.JWT_SECRET as string,
-    { expiresIn: "1h" },
-  );
-
-  return NextResponse.json(
-    { message: "User created successfully", token },
-    { status: 201 },
-  );
 }
