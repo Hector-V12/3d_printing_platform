8685658d38131b5a01699f0a415c3726b1ca0a16
faithfuldao <victor.dubrana2@gmail.com>
2024-06-09 23:48:02 +0200

    translations

--- lib/translation.ts
+++ lib/translation.ts
@@ -1,10 +1,38 @@
 export interface Translation {
   userName: string;
+  name: string;
   password: string;
   connexion: string;
   greeting: string;
   footerText: string;
   mailIsep: string;
+  register: string;
+  phoneNumber: string;
+  orderHistory: string;
+  commandTitle: string;
+  commandTitleField: string;
+  quantity: string;
+  printingTechno: string;
+  matChoice: string;
+  matType: string;
+  File: string;
+  remark: string;
+  fileImport: string;
+  fileImportFail: string;
+  modelHelp: string;
+  addToCart: string;
+  pendingOrder: string;
+  order: string;
+  account: string;
+  paymentInfo: string;
+  cardNumber: string;
+  expirationDate: string;
+  adress: string;
+  city: string;
+  commandRecap: string;
+  ou: string;
+  notification: string;
+  surname: string;
 }
 
 export type Language = "en" | "fr" | "es" | "zh";
