691a7958aa28f4de8fbc915e9eebfb171b9d6e6b
faithfuldao <victor.dubrana2@gmail.com>
2024-06-10 08:50:45 +0200

    previsu 3D

---
+++ src/app/Preview/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import React from "react";
+
+import { ModelViewer } from "../_components/modelViewer";
+
+export default function Preview() {
+  return (
+    <div>
+      <ModelViewer />
+    </div>
+  );
+}
